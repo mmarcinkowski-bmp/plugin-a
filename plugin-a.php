22d6768c56e30828b5cce553fecada392411e555
mmarcinkowski <marcin.marcinkowski@burdamedia.pl>
2025-01-23 16:57:17 +0100

    init plugin

---
+++ plugin-a.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Plugin Name: Plugin A
+ * Description: Test Plugin A
+ * Version: 1.0.0
+ * Author: Marcin M
+ * License: GPL v2
+ */
+
+function displayPluginAPage() {
+    echo 'Plugin A';
+}
+function addPluginAMenu() {
+    add_menu_page(
+        'Plugin A',// page title
+        'Plugin A',// menu title
+        'manage_options',// capability
+        'plugin-a',// menu slug
+        'displayPluginAPage' // callback function
+    );
+}
+add_action('admin_menu', 'addPluginAMenu');
