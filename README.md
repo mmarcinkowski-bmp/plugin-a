4c83640859220009c300b2d8546a749b069a3e9b
mmarcinkowski <marcin.marcinkowski@burdamedia.pl>
2025-06-12 14:48:46 +0200

    feat: plugin release config

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Plugin A
 
 Test plugin A
+
+
+```
+npm install
+export GH_TOKEN=your_token_here
+npx semantic-release --dry-run
+
+```
---
+++ release.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    branches: ['main'],
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        ['@semantic-release/changelog', {
+            changelogFile: 'CHANGELOG.md',
+        }],
+        ['@semantic-release/git', {
+            assets: ['CHANGELOG.md', 'plugin-a.php'],
+            message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+        }],
+        '@semantic-release/github',
+    ],
+};
