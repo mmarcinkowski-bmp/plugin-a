11368c901785793e3f22e8c755fb22b3886258a9
mmarcinkowski <marcin.marcinkowski@burdamedia.pl>
2025-06-12 16:06:56 +0200

    feat: try update plugin file version

--- release.config.js
+++ release.config.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 module.exports = {
     branches: ['main'],
     tagFormat: '${version}', // disables v prefix
@@ -7,6 +9,16 @@ module.exports = {
         ['@semantic-release/changelog', {
             changelogFile: 'CHANGELOG.md',
         }],
+        // update version in plugin file
+        {
+            prepare: (pluginConfig, context) => {
+                const filePath = './plugin-a.php';
+                const contents = fs.readFileSync(filePath, 'utf8');
+                const updated = contents.replace(/Version:\s*[0-9.]+/, `Version: ${context.nextRelease.version}`);
+                fs.writeFileSync(filePath, updated);
+                context.logger.log(`Updated plugin version to ${context.nextRelease.version}`);
+            }
+        },
         ['@semantic-release/git', {
             assets: ['CHANGELOG.md', 'plugin-a.php'],
             message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
